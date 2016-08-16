2fef823c1a6ef1ec99bf9cb54ecfe11f006accf6
Lou Zell <lzell11@gmail.com>
2016-08-16 07:46:36 -0700

    Workspace containing two projects, problem:
    Go to Project Navigator > Hello App > Build Phases and tap the plus
    sign next to Target Dependencies.  Note that the framework target is
    not presented as an option

---
+++ HelloFramework/HelloFramework/HelloFramework.h
@@ -0,0 +1,19 @@
+//
+//  HelloFramework.h
+//  HelloFramework
+//
+//  Created by Lou Zell on 8/16/16.
+//  Copyright © 2016 Lou Zell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloFramework.
+FOUNDATION_EXPORT double HelloFrameworkVersionNumber;
+
+//! Project version string for HelloFramework.
+FOUNDATION_EXPORT const unsigned char HelloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloFramework/PublicHeader.h>
+
+
